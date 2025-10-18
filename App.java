7f9ca4093013618c1652b3fe6bde7a19d3b8797d
ljg5489 <ljg5489@naver.com>
2025-10-18 22:37:09 +0900

    App 클래스 추가

--- App.java
+++ App.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.io.*;
 import java.util.*;
 /**
@@ -32,16 +31,4 @@ public class App
         lbMS.printLoanList();
         
     }
-=======
-
-/**
- * App 클래스의 설명을 작성하세요.
- *
- * @author (작성자 이름)
- * @version (버전 번호 또는 작성한 날짜)
- */
-public class App
-{
-    
->>>>>>> b020fd619e788afea889dc141c54c8ce0d7143db
 }
