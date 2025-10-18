898c915c39c8cf9390f6723ab74ecb36784a226b
ljg5489 <ljg5489@naver.com>
2025-10-18 22:32:40 +0900

    App 클래스

---
+++ App.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+/**
+ * App 클래스의 설명을 작성하세요.
+ *
+ * @author (2022320005 이진규)
+ * @version (2025.10.18)
+ */
+public class App
+{
+    /**
+     * main 메소드 - 도서관관리시스템의 메인 실행 메소드입니다.
+     *
+     */
+    public static void main(String[] args)
+    {
+        LibraryManagementSystem lbMS = new LibraryManagementSystem();
+        
+        HashMap<User,Book> loanDB = new HashMap<>();
+        
+        lbMS.setUserDB("C\\Temp\\UserData2025.txt");
+        
+        lbMS.printDB(userDB);
+        
+        lbMS.setBookDB("c\\Temp\\BookData2025.txt");
+        
+        lbMS.printDB(userDB);
+        
+        lbMS.borrowBook("2025230001","B02");
+        
+        lbMS.printLoanList();
+        
+    }
+}
