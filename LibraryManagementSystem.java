bc2d834ece22b7336652a518bdf65cdd5ddd0d04
hakyaung <hk100456@naver.com>
2025-10-20 01:30:59 +0900

    borrowBook 메소드, printDB 메소드, setUserDB 메소드 구현

--- LibraryManagementSystem.java
+++ LibraryManagementSystem.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.io.*;
 import myClass.*;
+import DataBase.*;
 /**
  * LybraryManagementSystem 클래스의 설명을 작성하세요.
  *
@@ -20,15 +21,21 @@ public class LibraryManagementSystem
     }
     
     public void borrowBook(String userID, String bookID){
-        
+        User user = userDB.findElement(userID);
+        Book book = bookDB.findElement(bookID);
+        loanDB.put(user,book);
     }
     
-    public void printDB(LibDB<T> db){
-        
+    public <T extends DB_Element> void printDB(LibDB<T> db){
+        db.printAllElements();
     }
     
     public void printLoanList(){
-        
+        Set<User> user = loanDB.keySet();
+        Iterator<User> it = user.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next() + "===>" + loanDB.get(it));
+        }
     }
     
     public LibDB<Book> setBookDB(String bookFile){
@@ -72,9 +79,35 @@ public class LibraryManagementSystem
         }catch(IOException e){
             System.out.println("파일 경로가 올바르지 않습니다.");
         }
+        return bookDB;
     }
     
     public LibDB<User> setUserDB(String userFile){
+        String str = "";
         
+        try{
+            FileReader src = new FileReader(userFile);
+            Scanner sc = new Scanner(src);
+            while(sc.hasNext()){
+                str = sc.nextLine();
+                StringTokenizer stz = new StringTokenizer(str, "/");
+                String name = "";
+                Integer stID = 0;
+                int stz_i = stz.countTokens();
+                int j = 0;
+                for(int i=0;i<stz_i;i++){
+                    if(i==0){
+                        stID = Integer.parseInt(stz.nextToken());
+                    }else if(i==1){
+                        name = stz.nextToken();
+                    }
+                }
+                User u = new User(name, stID);
+                userDB.addElement(u);
+            }
+        }catch(IOException e){
+            System.out.println("파일 경로가 올바르지 않습니다.");
+        }
+        return userDB;
     }
 }
