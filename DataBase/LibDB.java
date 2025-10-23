c4000aa57567c480e944eb5954a8fe6ce5d75033
ljg5489 <ljg5489@naver.com>
2025-10-23 15:48:16 +0900

    주석문 수정 완료

--- DataBase/LibDB.java
+++ DataBase/LibDB.java
@@ -6,7 +6,7 @@ import java.util.Iterator; // 7장: 컬렉션 프레임워크 (Iterator)
 
 
 /**
- * LibDB 클래스의 설명을 작성하세요. User객체들이나 Book객체들을 List로 관리하는 제네릭 데이터베이스 클래스입니다.
+ * LibDB 클래스의 설명을 작성하세요. User객체들이나 Book객체들을 List로 관리하는 제네릭 데이터베이스 클래스입니다.
  *
  * @author (20220320011_김민겸)
  * @version (2025.10.19)
@@ -14,8 +14,8 @@ import java.util.Iterator; // 7장: 컬렉션 프레임워크 (Iterator)
 
 
 /**
-* 제네릭 클래스를 작성하는 방법은 기전의 클래스 작성 방법과
-* 교과서 436페이지 제네릭 클래스 참고
+* 제네릭 클래스를 작성하는 방법은 기전의 클래스 작성 방법과
+* 교과서 436페이지 제네릭 클래스 참고
 */
 public class LibDB<T extends DB_Element>
 {
--- myClass/DB_Element.java
+++ myClass/DB_Element.java
@@ -2,7 +2,7 @@ package myClass;
 
 
 /**
- * DB_Element 클래스의 설명을 작성하세요. User클래스외 Book클래스의 슈퍼 클래스가 되는 추상 클래스입니다.
+ * DB_Element 클래스의 설명을 작성하세요. User클래스외 Book클래스의 슈퍼 클래스가 되는 추상 클래스입니다.
  *
  * @author (20220320011_김민겸)
  * @version (2025.10.19)
@@ -10,13 +10,13 @@ package myClass;
 
 
 /**
- * 추상 클래스가 되는 경우는 모두 abstract 키워드로 선언해야 한다.
+ * 추상 클래스가 되는 경우는 모두 abstract 키워드로 선언해야 한다.
  * 교과서 297페이지 추상 클래스 만들기 참고
  */
 public abstract class DB_Element
 {
     /**
-     * 추상 메소드란 선언은 돼있으나 코드가 구현되지 않은 껍데기만 있는 메소드
+     * 추상 메소드란 선언은 돼있으나 코드가 구현되지 않은 껍데기만 있는 메소드
      * 교과서 296페이지 추상 메소드 참고 
      */
     public abstract String getID(); 
