c59e90d7ca87f13f5ec2ce8bc75c005041b8a0a1
ljg5489 <ljg5489@naver.com>
2025-10-23 16:19:45 +0900

    주석문 수정

--- DataBase/LibDB.java
+++ DataBase/LibDB.java
@@ -14,9 +14,9 @@ import java.util.Iterator;
 
 
 /**
-* 제네릭 클래스를 작성하는 방법은 기전의 클래스 작성 방법과 유사한데,
-* 클래스 이름 다음에 일반화된 타입의 맥변수를 <> 사이에 츄가한다는 차이가 있다
-* 교과서 436페이지 제네릭 클래스 참고
+* 제네릭 클래스를 작성하는 방법은 기전의 클래스 작성 방법과 유사한데,
+* 클래스 이름 다음에 일반화된 타입의 맥변수를 <> 사이에 추가한다는 차이가 있다
+* 교과서 436페이지 제네릭 클래스 참고
 * 제네릭 클래스를 작성하는 방법은 기전의 클래스 작성 방법과
 * 교과서 436페이지 제네릭 클래스 참고
 * 제네릭 클래스를 작성하는 방법은 기전의 클래스 작성 방법과
@@ -39,8 +39,8 @@ public class LibDB<T extends DB_Element>
     public T findElement(String s)
     {
         /**
-         * 저장된 컬렉션에서 요소를 순차 검색할 때 java.util 패키지의 Iterator<E> 인터페이스를 사용하면 편리하다
-         * 교과서 422페이지 컬렉션의 순차 건색을 위한 Iterator 참고
+         * 저장된 컬렉션에서 요소를 순차 검색할 때 java.util 패키지의 Iterator<E> 인터페이스를 사용하면 편리하다
+         * 교과서 422페이지 컬렉션의 순차 건색을 위한 Iterator 참고
          */
         Iterator<T> it = db.iterator();
         while (it.hasNext())
@@ -57,7 +57,7 @@ public class LibDB<T extends DB_Element>
     public void printAllElements()
     {
         /**
-         * 배열이나 나열의 크기만큼 루프를 돌면서 각 원소를 순차적으로 접근하는데 유용하게 만든 for 문을 for-each문
+         * 배열이나 나열의 크기만큼 루프를 돌면서 각 원소를 순차적으로 접근하는데 유용하게 만든 for문을 for-each문
          * 교과서 137페이지 배열과 for-each문 참고
          */
         for (T element : db)
